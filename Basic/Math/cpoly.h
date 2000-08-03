43e1b6d93edf191e2eee4ab20f1bfd028b9b9417
Douglas Burke <dburke@cfa.harvard.edu>
2000-08-03 01:13:57 +0000

    Improved "demo bad". Updated to match recent main branch changes

---
+++ Basic/Math/cpoly.h
@@ -0,0 +1,14 @@
+#ifdef DEBUGMAIN
+void prtc(int n, double p[], double q[]);
+void prtz(int n,double zr[], double zi[]);
+#endif
+int cpoly(double opr[], double opi[], int degree,
+	   double zeror[], double zeroi[]);
+
+#if !defined(FALSE)
+#define FALSE (0)
+#endif
+#if !defined(TRUE)
+#define TRUE (1)
+#endif
+
