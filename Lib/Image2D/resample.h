39049f4544a40ffab4f563a95bad599011a127be
Douglas Burke <dburke@cfa.harvard.edu>
2000-10-18 05:25:32 +0000

    Added warp2d (+ utility fns) to PDL::Image2D - polynomial warping of images

---
+++ Lib/Image2D/resample.h
@@ -0,0 +1,36 @@
+/*
+ * resample.h - based on code from version 3.6-0 of the Eclipse library (ESO)
+ *   by Nicolas Devillard
+ *
+ * see http://www.eso.org/eclipse for further details
+ */
+
+#ifndef _PDL_RESAMPLE_H_
+#define _PDL_RESAMPLE_H_
+
+#include <math.h>
+
+#ifndef NULL
+#define NULL (0L)
+#endif
+
+/* Number of tabulations in kernel  */
+#define TABSPERPIX      (1000)
+#define KERNEL_WIDTH    (2.0)
+#define KERNEL_SAMPLES  (1+(int)(TABSPERPIX * KERNEL_WIDTH))
+
+#define TANH_STEEPNESS	(5.0)
+
+#ifndef PI_NUMB
+#define PI_NUMB     (3.1415926535897932384626433832795)
+#endif
+
+/* declare functions */
+
+double
+poly2d_compute( int ncoeff, double *c, double u, double *vpow );
+
+double   *
+generate_interpolation_kernel(char * kernel_type);
+
+#endif
