00e4c5920fb7d3c4cc095f782588baab87a49567
Tim Jenness <t.jenness@jach.hawaii.edu>
2002-06-13 20:00:36 +0000

    revert printf now that perl5.8.0RC2 prints doubles correctly

--- Basic/Core/pdlapi.c
+++ Basic/Core/pdlapi.c
@@ -501,7 +501,7 @@ void pdl_dump_spac(pdl *it,int nspac)
 	if(it->state & PDL_ALLOCATED) {
 		printf(")\n%s   First values: (",spaces);
 		for(i=0; i<it->nvals && i<10; i++) {
-			printf("%s%f",(i?" ":""),(NV)pdl_get_offs(it,i));
+			printf("%s%f",(i?" ":""),pdl_get_offs(it,i));
 		}
 	} else {
 		printf(")\n%s   (not allocated",spaces);
