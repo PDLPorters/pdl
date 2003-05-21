f26f3f736fcdbba066d8fec2879156062ebe2464
Craig DeForest <deforest@boulder.swri.edu>
2003-05-21 20:35:04 +0000

    Removed reference to nonexistent man page

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -9,10 +9,12 @@ scientific and bulk numeric data processing and display.  It extends
 perl's syntax and includes fully vectorized, multidimensional array
 handling, plus several paths for device-independent graphics output.
 
+For basic information on the PDL language, see L<the pdl(1)
+(lowercase) man page|pdl>.
+
 You can run PDL programs directly as perl scripts that include the
 PDL module (with "use PDL;"), or via an interactive shell (see
-L<the perldl(1) man page|perldl>).  For basic information on the 
-language, see L<the pdl(1) (lowercase) man page|pdl>.
+L<the perldl(1) man page|perldl>).  
 
 The PDL language extension includes about a dozen perl modules that
 form the core of the language, plus additional modules that add
@@ -26,9 +28,6 @@ for versions later than 2.3.1. Note that PDL::Math is still
 I<not> included in the
 L<Lite|PDL::Lite> and L<LiteF|PDL::LiteF>
 start-up modules.
-
-If you are looking for general documentation on PDL
-the L<PDL::Index|PDL::Index> manpage is a good starting point.
 
 =head1 SYNOPSIS
 
