7d02d7ceeaa84dcbeea29d5a9ac9c184a59cbe2a
Douglas Burke <dburke@cfa.harvard.edu>
2006-06-20 15:56:33 +0000

    Hopefully a saner way to build Basic/MatrixOps

--- Basic/MatrixOps/complex.h
+++ Basic/MatrixOps/complex.h
@@ -3,6 +3,9 @@
  * (C) Copyright 2001 by NetGroup A/S. All rights reserved.
  *
  * $Log$
+ * Revision 1.1  2006/06/20 15:57:22  djburke
+ * Hopefully a saner way to build Basic/MatrixOps
+ *
  * Revision 1.1  2005/01/08 09:22:57  zowie
  * Added non-symmetric matrices to eigens; updated version to 2.4.2cvs.
  *
