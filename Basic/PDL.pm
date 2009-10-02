591a9cca7904515e65dc7d36f0a904a12ccb0fbc
Chris Marshall <devel.chm.01@gmail.com>
2009-10-02 12:48:34 -0400

    Update version to 2.4.4_12_1 for development
    With the rapid releases, we need a unique identifier for
    each git push to try.  See if adding one more digit rather
    than git works.  The plan is to update the last digit for
    each push to sf.net so there is a version "paper trail"
    for reference and debugging.

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -73,7 +73,7 @@ start-up modules.
 
 
 # set the version:
-$PDL::VERSION = '2.4.4_12git'; # Remember to remove the 'git' for releases...
+$PDL::VERSION = '2.4.4_12_1'; # Remember to remove the 'git' for releases...
 
 # Main loader of standard PDL package
 
