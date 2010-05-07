5402c258d081efadd8cad0f152947f98405ceb8d
Chris Marshall <devel.chm.01@gmail.com>
2010-05-06 23:05:30 -0400

    Update VERSION to 2.4.6_007 post CPAN dev release
    ...for ongoing development to continue.

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -73,7 +73,7 @@ start-up modules.
 
 
 # set the version:
-$PDL::VERSION = '2.4.6_006'; # Go to sub numbering per git push
+$PDL::VERSION = '2.4.6_007'; # Go to sub numbering per git push
 
 # Main loader of standard PDL package
 
