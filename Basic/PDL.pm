3f7962da74f4fc43871fe57027e82278eff3a175
Chris Marshall <devel.chm.01@gmail.com>
2010-07-07 18:03:45 -0400

    Update VERSION to 2.4.6_014 for development

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -109,7 +109,7 @@ start-up modules.
 
 
 # set the version:
-$PDL::VERSION = '2.4.6_013'; # Go to sub numbering per git push
+$PDL::VERSION = '2.4.6_014'; # Go to sub numbering per git push
 
 # Main loader of standard PDL package
 
