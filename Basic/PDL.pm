fca8547e64da376af0e234d700b155ce208464fd
Daniel Carrera <dcarrera@gmail.com>
2010-08-13 12:41:20 +0200

    Delete Intro.pod and introduce Tutorials.pod in its place.

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -23,25 +23,28 @@ C<help>.
 
 =head1 LANGUAGE DOCUMENTATION
 
-To learn about the PDL language see:
+Most PDL documentation describes the language features. The number of
+PDL pages is too great to list here. The following pages offer some
+guidance to help you find the documentation you need.
+
 
 =over 5
 
-=item L<PDL::Intro|PDL::Intro>
+=item L<PDL::FAQ|PDL::FAQ>
 
-Starting place for PDL language documentation.
+Frequently asked questions about PDL. This page covers a lot of
+questions that do not fall neatly into any of the documentation
+categories.
 
-=item L<PDL::QuickStart|PDL::QuickStart>
+=item L<PDL::Tutorials|PDL::Tutorials>
 
-A guide to get you quickly started with PDL.
-
-=item L<PDL::Philosophy|PDL::Philosophy>
-
-Why did we write PDL? Learn about what PDL has to offer.
+A guide to PDL's tutorial-style documentation. With topics from beginner
+to advanced, these pages teach you various aspects of PDL step by step.
 
 =item L<PDL::Index|PDL::Index>
 
-List of all available documentation.
+List of all available documentation, sorted alphabetically. If you
+cannot find what you are looking for, try here.
 
 =back
 
