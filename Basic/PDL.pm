eca3468166b4f0a1b6ec2118ce4eb869eca6eb03
Chris Marshall <devel.chm.01@gmail.com>
2010-08-15 19:32:55 -0400

    Fix .perldlrc double require bug in perldl
    There was a problem where running 'perl perldl' after building
    PDL from the source directory would only give you plain perl.
    The code should have been retrying with 'use blib' so things
    should have worked since 'perl -Mblib perldl' does work.
    
    The subtlety was that require caches the names of files that
    it fails to load in %INC as well as ones that work.  To attempt
    again, you have to delete the previously created hash entry.
    
    Also updated version for next pre-release and some documenting
    comments in perldl.conf

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -139,7 +139,7 @@ start-up modules.
 
 
 # set the version:
-$PDL::VERSION = '2.4.6_991'; # Go to sub numbering per git push
+$PDL::VERSION = '2.4.6_992'; # Go to sub numbering per git push
 
 # Main loader of standard PDL package
 
