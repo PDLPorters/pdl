9cf9e49b396f854aeedd6889cd5a854fa2030e22
Craig DeForest <zowie@Clio.local>
2010-11-04 01:12:06 -0600

    Add synonym t_zenithal to t_vertical in Cartography.pm

--- Lib/Transform/Cartography/Cartography.pm
+++ Lib/Transform/Cartography/Cartography.pm
@@ -2276,7 +2276,7 @@ sub t_hammer {
 
 ######################################################################
 
-=head2 t_vertical
+=head2 t_vertical, t_zenithal -- vertical perspective projection
 
 =for usage
 
@@ -2289,7 +2289,10 @@ sub t_hammer {
 Vertical perspective projection is a generalization of L<gnomonic|/t_gnomonic>
 and L<stereographic|/t_stereographic> projection, and a special case of 
 L<perspective|/t_perspective> projection.  It is a projection from the 
-sphere onto a focal plane at the camera location.  
+sphere onto a tangent plane from a point at the camera location.
+
+Vertical projections are also called "zenithal", and t_zenithal is an 
+alias for t_vertical.
 
 OPTIONS
 
@@ -2470,6 +2473,8 @@ sub t_vertical {
 
     $me->_finish;
   }
+
+*t_zenithal = \&t_vertical;
 
 ######################################################################
 
