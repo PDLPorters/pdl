5368b77235b6cb8a795d75397dbf5f3f9e1c9f4e
Chris Marshall <devel.chm.01@gmail.com>
2010-11-11 21:41:10 -0500

    Add POD for wmpeg status return values.

--- IO/Pnm/Pic.pm
+++ IO/Pnm/Pic.pm
@@ -688,19 +688,22 @@ Write an image sequence ((3,x,y,n) piddle) as an MPEG animation.
 
 =for example
 
-   $anim->wmpeg("GreatAnimation.mpg");
+  $anim->wmpeg("GreatAnimation.mpg")
+      or die "can't create mpeg output";
 
 Writes a stack of rgb images as an mpeg movie. Expects
 a 4-D pdl of type byte as input. First dim has to be
-3 since it is interpreted as interlaced RGB. Some of
-the input data restrictions will have to be relaxed in
-the future but routine serves as a proof of principle
-at the moment. It uses the program ffmpeg to encode
-the frames into video. The arguments and parameters
-used for ffmpeg have not been tuned. This is a first
-implementation replacing mpeg_encode by ffmpeg.
+3 since it is interpreted as interlaced RGB. It returns
+1 on success and undef on failure.
+
+Some of the input data restrictions will have to be
+relaxed in the future but routine serves as a proof of
+principle at the moment.  It uses the program ffmpeg
+to encode the frames into video.  The arguments and
+parameters used for ffmpeg have not been tuned.  This is
+a first implementation replacing mpeg_encode by ffmpeg.
 Currently, wmpeg doesn't allow modification of the
-parameters written through its calling interface. This
+parameters written through its calling interface.  This
 will change in the future as needed.
 
 In the future it might be much nicer to implement
@@ -708,10 +711,10 @@ a movie perl object that supplies methods for
 manipulating the image stack (insert, cut, append
 commands) and a final movie->make() call would invoke
 ffmpeg on the picture stack (which will only be held on
-disk). This should get around the problem of having to
+disk).  This should get around the problem of having to
 hold a huge amount of data in memory to be passed into
-wmpeg (when you are, e.g. writing a large animation
-from PDL3D rendered fly-throughs). Having said that,
+wmpeg (when you are, e.g.  writing a large animation
+from PDL3D rendered fly-throughs).  Having said that,
 the actual storage requirements might not be so big
 in the future any more if you could pass 'virtual'
 transform pdls into wmpeg that will only be actually
