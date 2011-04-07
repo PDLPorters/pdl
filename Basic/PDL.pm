083a2fdf28a2aa2423f9569a040ca686180d0fe0
Chris Marshall <devel.chm.01@gmail.com>
2011-04-07 16:08:15 -0400

    Update VERSION and readme/notes for 2.4.9
    The PDL-2.4.9 release is planned for 10 April 2011.

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -139,7 +139,7 @@ start-up modules.
 
 
 # set the version:
-$PDL::VERSION = '2.4.8_004'; # Go to sub numbering per git push
+$PDL::VERSION = '2.4.9'; # Go to sub numbering per git push
 
 # Main loader of standard PDL package
 
