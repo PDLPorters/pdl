d6c4086fc887c5e42f4fc5d1ead6e6faf522ff17
Derek Lamb <lambd@users.sourceforge.net>
2011-11-14 16:00:39 -0700

    Perldl2/Script.pm: Wrap qw(...) in parens to deal with Perl 5.13.6 deprecation.
    Loading pdl2 gave warnings that "Use of qw(...) as parentheses is deprecated".  See http://search.cpan.org/~miyagawa/perl-5.13.6/pod/perl5135delta.pod#Deprecations

--- Perldl2/Script.pm
+++ Perldl2/Script.pm
@@ -25,7 +25,7 @@ sub load_rcfile {
 
    # get rc file name
    my $startup_file = _startup_def();
-   foreach my $startup_suffix qw( .pdlrc .perldlrc ) {
+   foreach my $startup_suffix (qw( .pdlrc .perldlrc )) {
       if ( -e "$HOME/$startup_suffix" ) {
          $startup_file = "$HOME/$startup_suffix";
          last;
@@ -35,7 +35,7 @@ sub load_rcfile {
    $self->apply_script($startup_file);
 
    # load local.perldlrc if it exists
-   foreach my $local_startup_file qw( local.pdlrc local.perldlrc ) {
+   foreach my $local_startup_file (qw( local.pdlrc local.perldlrc )) {
       if ( -e $local_startup_file ) {
          print STDERR "load_rcfile: loading $local_startup_file\n";
          $self->apply_script($local_startup_file);
