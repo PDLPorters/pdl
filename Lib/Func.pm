44902ed9a73f07c54947d8e64afc8d282b796911
Derek Lamb <lambd@users.sourceforge.net>
2012-11-09 13:59:25 -0700

    Small POD update for PDL::Func
    The description "useful functions" for this module wasn't that useful,
    so I added more detail.  Fixed a syntax error in the docs for
    PDL::Func::set.  Also removed the section "The Future" because
    obviously that isn't going to happen.

--- Lib/Func.pm
+++ Lib/Func.pm
@@ -1,7 +1,7 @@
 
 =head1 NAME
 
-PDL::Func - useful functions
+PDL::Func - interpolation, integration, & gradient estimation (differentiation) of functions
 
 =head1 SYNOPSIS
 
@@ -480,8 +480,8 @@ sub _get_value {
 
 =for usage
 
- my $nset = $obj->set( x => $newx, $y => $newy );
- my $nset = $obj->set( { x => $newx, $y => $newy } );
+ my $nset = $obj->set( x => $newx, y => $newy );
+ my $nset = $obj->set( { x => $newx, y => $newy } );
 
 =for ref
 
@@ -932,11 +932,6 @@ to form C<PDL::Func>. Comments greatly appreciated on the
 current implementation, as it is not too sensible.
 
 Thanks to Robin Williams, Halldór Olafsson, and Vince McIntyre.
-
-=head1 THE FUTURE
-
-Robin is working on a new version, that improves on the current version
-a lot. No time scale though!
 
 =head1 AUTHOR
 
