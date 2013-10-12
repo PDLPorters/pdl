02770bfd7aa75ca82499b34942cd014e932a1d09
Chris Marshall <devel.chm.01@gmail.com>
2013-10-12 13:54:49 -0400

    Update VERSION to 2.007_01 for development

--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -146,7 +146,7 @@ start-up modules.
 
 
 # set the version:
-$PDL::VERSION = '2.007';     # Go to sub numbering per git push
+$PDL::VERSION = '2.007_01';     # Go to sub numbering per git push
 
 # Main loader of standard PDL package
 
