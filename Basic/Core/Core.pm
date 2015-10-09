701a19533bc032ea74fde6b63024885b162e7185
Chris Marshall <devel.chm.01@gmail.com>
2015-10-09 02:55:17 -0400

    Update VERSION etc for CPAN release

--- Basic/Core/Core.pm
+++ Basic/Core/Core.pm
@@ -8,7 +8,7 @@ use PDL::Exporter;
 require PDL; # for $VERSION
 use DynaLoader;
 our @ISA    = qw( PDL::Exporter DynaLoader );
-our $VERSION = '2.013_04';
+our $VERSION = '2.013_05';
 bootstrap PDL::Core $VERSION;
 use PDL::Types ':All';
 use Config;
--- Basic/PDL.pm
+++ Basic/PDL.pm
@@ -147,7 +147,7 @@ start-up modules.
 
 
 # set the version:
-$PDL::VERSION = '2.013_04';
+$PDL::VERSION = '2.013_05';
 
 # Main loader of standard PDL package
 
