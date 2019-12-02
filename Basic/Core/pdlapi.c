89c52d662aabd9db6aa4f323cd5de746d90810b9
Ed J <mohawk2@users.noreply.github.com>
2019-12-02 00:48:39 +0000

    memset after malloc in pdlapi.c
    Fixes error found using:
    touch Basic/Ufunc/ufunc.pd; make core && valgrind --tool=memcheck --track-origins=yes -v perl -Mblib t/ufunc.t |& less

--- Basic/Core/pdlapi.c
+++ Basic/Core/pdlapi.c
@@ -117,6 +117,7 @@ pdl* pdl_create(int type) {
      if(type == PDL_TMP) {croak("PDL internal error. FIX!\n");}
 
      it = (pdl*) malloc(sizeof(pdl));
+     memset(it, 0, sizeof(pdl));
      if (it==NULL)
         croak("Out of Memory\n");
 
