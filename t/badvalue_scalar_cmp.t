PDLPorters/pdla-core

less spew from t/badvalue_scalar_cmp.t, test warns

t/badvalue_scalar_cmp.t
@@ -33,9 +33,17 @@ subtest "Issue example code" => sub {
 	is( "$m", 2, "Mean of [1 2 3] is 2" );
 	is( "$s", 1, "And std. dev is 1" );
 
+	if ($PDLA::Config{BADVAL_PER_PDLA}) {
+	  # to ensure warnings happen if per-PDLA
+	  $s->badflag(1);
+	  $s->badvalue(0);
+	}
+	my @warnings;
+	local $SIG{__WARN__} = sub { push @warnings, @_ };
 	is( "".($s >  0), "1", "is 1 >  0? -> true" );
 	is( "".($s <  0), "0", "is 1 <  0? -> false");
 	is( "".($s == 0), "0", "is 1 == 0? -> false");
+	ok scalar(@warnings), 'bad gave warnings';
 };
 
 subtest "Badvalue set on 0-dim PDLA + comparision operators" => sub {

PDLPorters/pdl

t/badvalue_scalar_cmp.t
@@ -33,9 +33,17 @@ subtest "Issue example code" => sub {
 	is( "$m", 2, "Mean of [1 2 3] is 2" );
 	is( "$s", 1, "And std. dev is 1" );
 
+	if ($PDL::Config{BADVAL_PER_PDL}) {
+	  # to ensure warnings happen if per-PDL
+	  $s->badflag(1);
+	  $s->badvalue(0);
+	}
+	my @warnings;
+	local $SIG{__WARN__} = sub { push @warnings, @_ };
 	is( "".($s >  0), "1", "is 1 >  0? -> true" );
 	is( "".($s <  0), "0", "is 1 <  0? -> false");
 	is( "".($s == 0), "0", "is 1 == 0? -> false");
+	ok scalar(@warnings), 'bad gave warnings';
 };
 
 subtest "Badvalue set on 0-dim PDL + comparision operators" => sub {