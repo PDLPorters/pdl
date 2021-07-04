45c68c6b7e206e9c8a0602616b1ef88476eb6811
Ed J <mohawk2@users.noreply.github.com>
2021-07-04 13:07:10 +0100

    add PDL::Opt::Simplex example - closes #319

--- Libtmp/Simplex/Simplex.pm
+++ Libtmp/Simplex/Simplex.pm
@@ -5,13 +5,45 @@ PDL::Opt::Simplex -- Simplex optimization routines
 
 =head1 SYNOPSIS
 
- use PDL::Opt::Simplex;
+  use PDL::Opt::Simplex;
 
- ($optimum,$ssize,$optval) = simplex($init,$initsize,$minsize,
+  ($optimum,$ssize,$optval) = simplex($init,$initsize,$minsize,
  		 $maxiter,
  		 sub {evaluate_func_at($_[0])},
  		 sub {display_simplex($_[0])}
  		 );
+
+  # more involved:
+  use PDL;
+  use PDL::Opt::Simplex;
+  use Data::Dumper;
+
+  my $count = 0;
+  # find value of $x that returns a minimum
+  sub f {
+    my ($vec) = @_;
+    $count++;
+    my $x = $vec->slice('(0)');
+    # The parabola (x+3)^2 - 5 has a minima at x=-3:
+    return (($x+3)**2 - 5);
+  }
+
+  sub log {
+    my ($vec, $vals, $ssize) = @_;
+    # $vec is the array of values being optimized
+    # $vals is f($vec)
+    # $ssize is the simplex size, or roughly, how close to being converged.
+    my $x = $vec->slice('(0)');
+    # each vector element passed to log() has a min and max value.
+    # ie: x=[6 0] -> vals=[76 4]
+    # so, from above: f(6) == 76 and f(0) == 4
+    print "$count [$ssize]: $x -> $vals\n";
+  }
+
+  my $vec_initial = pdl [30];
+  my ( $vec_optimal, $ssize, $optval ) = simplex($vec_initial, 3, 1e-6, 100, \&f, \&log);
+  my $x = $vec_optimal->slice('(0)');
+  print "ssize=$ssize  opt=$x -> minima=$optval\n";
 
 =head1 DESCRIPTION
 
