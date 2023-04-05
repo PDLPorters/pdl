ab67ad40724241ca8aafec2b8c93f1230c961ae6
Derek Lamb <lambd@users.sourceforge.net>
2023-04-05 15:50:22 -0600

    Use fabsl instead of fabs in resample.c (warp2d)
    Addresses argument/parameter type mismatch for long double.
    Compiler warned using fabs may cause truncation in value.

--- Libtmp/Image2D/resample.c
+++ Libtmp/Image2D/resample.c
@@ -300,7 +300,7 @@ generate_interpolation_kernel(char *kernel_type, int samples, long double *tab)
 	} else if (!strcmp(kernel_type, "lanczos")) {
 		for (i=0 ; i<samples ; i++) {
 			x = (long double)KERNEL_WIDTH * (long double)i/(long double)(samples-1) ;
-			if (fabs(x)<2) {
+			if (fabsl(x)<2) {
 				tab[i] = sinc(x) * sinc(x/2) ;
 			} else {
 				tab[i] = 0.00 ;
