9868a0fba3c8034df44de8ff520502d834a6526e
Ed J <mohawk2@users.noreply.github.com>
2024-06-27 12:52:28 +0100

    IFiles not actually need Inline loaded

--- Basic/Core/IFiles.pm
+++ Basic/Core/IFiles.pm
@@ -42,13 +42,11 @@ foreach (@INC) {
 sub deps { }
 # support: use Inline with => 'PDL';
 
-require Inline;
-
 sub Inline {
   my ($class, $lang) = @_;
   return {} if $lang eq 'Pdlpp';
-  unless($ENV{"PDL_Early_Inline"} // ($Inline::VERSION >= 0.68) ) {
-      die "PDL::Inline: requires Inline version 0.68 or higher to make sense\n  (yours is $Inline::VERSION). You should upgrade Inline, \n   or else set \$ENV{PDL_Early_Inline} to a true value to ignore this message.\n";
+  unless ($ENV{"PDL_Early_Inline"} || (($Inline::VERSION//0.68) >= 0.68)) {
+      die "PDL::Inline: requires Inline version 0.68 or higher to make sense\n  (yours is $Inline::VERSION). You should upgrade Inline,\n   or else set \$ENV{PDL_Early_Inline} to a true value to ignore this message.\n";
   }
   +{
     TYPEMAPS      => [ &PDL::Core::Dev::PDL_TYPEMAP ],
