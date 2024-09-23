66e1292f5a1c17277920cf78cc781d3a28d27ad1
Ed J <mohawk2@users.noreply.github.com>
2024-09-23 05:21:39 +0000

    fix Demos/earth-interp.pl including use P:G:Simple

--- Demos/earth-interp.pl
+++ Demos/earth-interp.pl
@@ -11,21 +11,22 @@ use PDL::NiceSlice;
 # Snarf the file and build a separate ndarray for each polyline
 print "Interpreting map file...\n";
 
-open(MAP,"<earth.txt");
+open my $mapfh, "<", "earth.txt";
 $nelem = 0;
-while(<MAP>) {
-  next if(m/^\#/ || m/^\s*$/);
+while(<$mapfh>) {
+  next if m/^\#/ || m/^\s*$/;
   s/^\s+//;
   ($x,$y,$z,$color) = split /\s+/;
-  if($color==0 and $#a > 1) {
-    push(@a,$a[0]);
-    my $c = $a[1]->[3];
+  if ($color==0 and @this_polyline > 2) {
+    push @this_polyline,$this_polyline[0];
+    my $c = $this_polyline[1]->[3];
     print $c;
-    ($a[-1])->[3] = $c;
+    ($this_polyline[-1])->[3] = $c;
     $nelem++;
-    push(@mainlist,pdl(@a)); undef @a;
+    push @mainlist,pdl(\@this_polyline);
+    @this_polyline = ();
   }
-  push(@a,[$x,$y,$z,$color ? $color-1:0]);
+  push @this_polyline,[$z,$x,$y,$color ? $color-1:0];
   $nelem++;
 }
 
@@ -36,7 +37,7 @@ foreach $z(@mainlist) {
   $n = $z->dim(1);
   $elements->(0:2,$pos:$pos+$n-1) .= $z->(0:2);
   $elements->((3),$pos:$pos+$n-2) .= $z->((3),1:-1);
-  $elements->((3),$pos+n-1) .= 0;
+  $elements->((3),$pos+$n-1) .= 0;
   $pos += $n;
 }
 
@@ -46,18 +47,21 @@ print "... $nelem vectors\n";
 use PDL::Transform;
 $t = t_compose(t_scale(ones(3)*180/3.14159),t_spherical());
 
-$lonlat = $t->apply( $elements->(pdl(2,0,1)) )  ->(1:2); # discard radius
+$lonlat = $t->apply( $elements(0:2) ) ->(0:1); # discard radius
 
 # Clean up the pen values at the longitude singularity
 print "cleaning up singularities...\n";
-$p = $elements->((3));
-$idx = which((abs($lonlat->((0),0:-2) - $lonlat->((0),1:-1))) > 355);
-$p->($idx) .= 0 if($idx->nelem > 0);
+use PDL::Transform::Cartography;
+$lonlatp = $lonlat->append($elements(3))->clean_lines;
 
 # Plot map
-use PDL::Graphics::PGPLOT::Window;
-$w = pgwin(dev=>'/xs',size=>[10,10]);
-$w->lines($lonlat,$p,{Title=>'Lat/Lon map of world coastlines',XTitle=>'East Longitude',YTitle=>'North Latitude',Axis=>2});
+use PDL::Graphics::Simple;
+$w = pgswin(size=>[8,4]);
+$w->plot(with=>'polylines', $lonlatp,
+  {Title=>'Lat/Lon map of world coastlines (Plate Carree)',
+    xlabel=>'East Longitude',ylabel=>'North Latitude'}
+);
+print "ret> "; <STDIN>;
 
-wfits($lonlat->glue(0,$p->dummy(0,1)),'earth_coast.vec.fits');
+#wfits($lonlatp,'earth_coast.vec.fits');
 1;
