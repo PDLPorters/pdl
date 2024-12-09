e8dca1ded6181349203876a16897822da93fcf75
Ed J <mohawk2@users.noreply.github.com>
2024-12-09 07:53:18 +0000

    split Libtmp/GSL out to separate PDL::GSL

--- Libtmp/GSL/gslerr.h
+++
@@ -1,8 +0,0 @@
-#include <gsl/gsl_errno.h>
-
-#define GSLERR(x,y) \
-  { \
-    int status = x y; \
-    if (status) \
-      return PDL->make_error(PDL_EUSERERROR, "Error in %s: %s", #x, gsl_strerror(status)); \
-  }
