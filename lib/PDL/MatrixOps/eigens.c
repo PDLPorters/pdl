2905c810fbd5a8e744235d41f5487a783c2e26ad
Ed J <mohawk2@users.noreply.github.com>
2024-12-27 21:16:12 +0000

    eigens_sym now sorts output like EISPACK

--- lib/PDL/MatrixOps/eigens.c
+++ lib/PDL/MatrixOps/eigens.c
@@ -42,6 +42,8 @@
  * ERROR MESSAGES:
  *
  * None.
+
+ Now orders output using code from TQL2 from EISPACK
  *
  */
 /*
@@ -51,127 +53,125 @@ Copyleft version.
 
 #include <math.h>
 
-void eigens( double A[], double RR[], double E[], int N )
-{
-int IND, L, LL, LM, M, MM, MQ, I, J, IA, LQ;
-int IQ, IM, IL, NLI, NMI;
-double ANORM, ANORMX, AIA, THR, ALM, ALL, AMM, X, Y;
-double SINX, SINX2, COSX, COSX2, SINCS, AIL, AIM;
-double RLI, RMI;
-static double RANGE = 1.0e-10; /*3.0517578e-5;*/
+void eigens( double A[], double EV[], double E[], int N ) {
+  int IND, L, LL, LM, M, MM, MQ, I, J, IA, LQ;
+  int IQ, IM, IL, NLI, NMI;
+  double ANORM, ANORMX, AIA, THR, ALM, ALL, AMM, X, Y;
+  double SINX, SINX2, COSX, COSX2, SINCS, AIL, AIM;
+  double RLI, RMI;
+  static double RANGE = 1.0e-10; /*3.0517578e-5;*/
 
+  /* Initialize identity matrix in EV[] */
+  for ( J=0; J<N*N; J++ )
+    EV[J] = 0.0;
+  MM = 0;
+  for ( J=0; J<N; J++ ) {
+    EV[MM + J] = 1.0;
+    MM += N;
+  }
 
-/* Initialize identity matrix in RR[] */
-for( J=0; J<N*N; J++ )
-	RR[J] = 0.0;
-MM = 0;
-for( J=0; J<N; J++ )
-	{
-	RR[MM + J] = 1.0;
-	MM += N;
-	}
+  ANORM=0.0;
+  for ( I=0; I<N; I++ ) {
+    for ( J=0; J<N; J++ ) {
+      if ( I != J ) {
+        IA = I + (J*J+J)/2;
+        AIA = A[IA];
+        ANORM += AIA * AIA;
+      }
+    }
+  }
+  if ( ANORM > 0.0 ) {
+    ANORM = sqrt( ANORM + ANORM );
+    ANORMX = ANORM * RANGE / N;
+    THR = ANORM;
 
-ANORM=0.0;
-for( I=0; I<N; I++ )
-	{
-	for( J=0; J<N; J++ )
-		{
-		if( I != J )
-			{
-			IA = I + (J*J+J)/2;
-			AIA = A[IA];
-			ANORM += AIA * AIA;
-			}
-		}
-	}
-if( ANORM <= 0.0 )
-	goto done;
-ANORM = sqrt( ANORM + ANORM );
-ANORMX = ANORM * RANGE / N;
-THR = ANORM;
+    while ( THR > ANORMX ) {
+      THR /= N;
 
-while( THR > ANORMX )
-{
-THR=THR/N;
+      do { /* while IND != 0 */
+        IND = 0;
 
-do
-{ /* while IND != 0 */
-IND = 0;
+        for ( L=0; L<N-1; L++ ) {
+          for ( M=L+1; M<N; M++ ) {
+            MQ=(M*M+M)/2;
+            LM=L+MQ;
+            ALM=A[LM];
+            if ( fabs(ALM) < THR )
+                    continue;
 
-for( L=0; L<N-1; L++ )
-	{
+            IND=1;
+            LQ=(L*L+L)/2;
+            LL=L+LQ;
+            MM=M+MQ;
+            ALL=A[LL];
+            AMM=A[MM];
+            X=(ALL-AMM)/2.0;
+            Y=ALM/sqrt(ALM*ALM+X*X);
+            if (X >= 0.0)
+                    Y=-Y;
+            SINX = Y / sqrt( 2.0 * (1.0 + sqrt( 1.0-Y*Y)) );
+            SINX2=SINX*SINX;
+            COSX2=1.0-SINX2;
+            COSX=sqrt(COSX2);
+            SINCS=SINX*COSX;
 
-for( M=L+1; M<N; M++ )
-	{
-	MQ=(M*M+M)/2;
-	LM=L+MQ;
-	ALM=A[LM];
-	if( fabs(ALM) < THR )
-		continue;
+            /*	   ROTATE L AND M COLUMNS */
+            for ( I=0; I<N; I++ ) {
+              IQ=(I*I+I)/2;
+              if ( (I != M) && (I != L) ) {
+                IM=(I > M)  ? M+IQ : I+MQ;
+                IL=(I >= L) ? L+IQ : I+LQ;
+                AIL=A[IL];
+                AIM=A[IM];
+                X=AIL*COSX-AIM*SINX;
+                A[IM]=AIL*SINX+AIM*COSX;
+                A[IL]=X;
+              }
+              NLI = N*L + I;
+              NMI = N*M + I;
+              RLI = EV[ NLI ];
+              RMI = EV[ NMI ];
+              EV[NLI]=RLI*COSX-RMI*SINX;
+              EV[NMI]=RLI*SINX+RMI*COSX;
+            }
 
-	IND=1;
-	LQ=(L*L+L)/2;
-	LL=L+LQ;
-	MM=M+MQ;
-	ALL=A[LL];
-	AMM=A[MM];
-	X=(ALL-AMM)/2.0;
-	Y=-ALM/sqrt(ALM*ALM+X*X);
-	if(X < 0.0)
-		Y=-Y;
-	SINX = Y / sqrt( 2.0 * (1.0 + sqrt( 1.0-Y*Y)) );
-	SINX2=SINX*SINX;
-	COSX=sqrt(1.0-SINX2);
-	COSX2=COSX*COSX;
-	SINCS=SINX*COSX;
+            X=2.0*ALM*SINCS;
+            A[LL]=ALL*COSX2+AMM*SINX2-X;
+            A[MM]=ALL*SINX2+AMM*COSX2+X;
+            A[LM]=(ALL-AMM)*SINCS+ALM*(COSX2-SINX2);
+          } /* for M=L+1 to N-1 */
+        } /* for L=0 to N-2 */
 
-/*	   ROTATE L AND M COLUMNS */
-for( I=0; I<N; I++ )
-	{
-	IQ=(I*I+I)/2;
-	if( (I != M) && (I != L) )
-		{
-		if(I > M)
-			IM=M+IQ;
-		else
-			IM=I+MQ;
-		if(I >= L)
-			IL=L+IQ;
-		else
-			IL=I+LQ;
-		AIL=A[IL];
-		AIM=A[IM];
-		X=AIL*COSX-AIM*SINX;
-		A[IM]=AIL*SINX+AIM*COSX;
-		A[IL]=X;
-		}
-	NLI = N*L + I;
-	NMI = N*M + I;
-	RLI = RR[ NLI ];
-	RMI = RR[ NMI ];
-	RR[NLI]=RLI*COSX-RMI*SINX;
-	RR[NMI]=RLI*SINX+RMI*COSX;
-	}
+      } while ( IND != 0 );
 
-	X=2.0*ALM*SINCS;
-	A[LL]=ALL*COSX2+AMM*SINX2-X;
-	A[MM]=ALL*SINX2+AMM*COSX2+X;
-	A[LM]=(ALL-AMM)*SINCS+ALM*(COSX2-SINX2);
-	} /* for M=L+1 to N-1 */
-	} /* for L=0 to N-2 */
+    } /* while THR > ANORMX */
+  }
 
-	}
-while( IND != 0 );
+  /* Extract eigenvalues from the reduced matrix */
+  L=0;
+  for ( J=0; J<N; J++ ) {
+    L=L+J+1;
+    E[J]=A[L-1];
+  }
 
-} /* while THR > ANORMX */
-
-done:	;
-
-/* Extract eigenvalues from the reduced matrix */
-L=0;
-for( J=1; J<=N; J++ )
-	{
-	L=L+J;
-	E[J-1]=A[L-1];
-	}
+  /* from TQL2, order eigenvalues and -vectors */
+  int II;
+  for ( II = 1; II < N; II++) {
+    I = II - 1;
+    int K = I;
+    double P = E[I];
+    for ( J = II; J < N; J++ ) {
+      if (E[J] >= P) continue;
+      K = J;
+      P = E[J];
+    }
+    if (K == I) continue;
+    E[K] = E[I];
+    E[I] = P;
+    for ( J = 0; J < N; J++ ) {
+       P = EV[ N*I+J ];
+       EV[ N*I+J ] = EV[ N*K+J ];
+       EV[ N*K+J ] = P;
+    }
+  }
 }
