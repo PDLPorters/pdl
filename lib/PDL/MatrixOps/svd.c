1e253b31cdd4f585c067f33588d749d7312f1209
Ed J <mohawk2@users.noreply.github.com>
2026-04-01 23:16:50 +0100

    fix svd.c indentation for comprehensibility

--- lib/PDL/MatrixOps/svd.c
+++ lib/PDL/MatrixOps/svd.c
@@ -116,98 +116,93 @@ void SVD(double *W, double *Z, int nRow, int nCol)
   RotCount = EstColRank*(EstColRank-1)/2;
 
   /*until convergence is achieved or too many sweeps are carried out*/
-  while (RotCount != 0 && SweepCount <= slimit)
-    {
-      RotCount = EstColRank*(EstColRank-1)/2; /* rotation counter */
-      SweepCount++;
-      for (j=0; j<EstColRank-1; j++) /* main cyclic Jacobi sweep */
-        {
-          for (k=j+1; k<EstColRank; k++)
-            {
-              p = q = r = 0.0;
-	      /*
-		Some helpful definitions from Nash's book (pg 34) to
-		understand this p, q, & r rotation business:
+  while (RotCount != 0 && SweepCount <= slimit) {
+    RotCount = EstColRank*(EstColRank-1)/2; /* rotation counter */
+    SweepCount++;
+    for (j=0; j<EstColRank-1; j++) { /* main cyclic Jacobi sweep */
+      for (k=j+1; k<EstColRank; k++) {
+        p = q = r = 0.0;
+        /*
+          Some helpful definitions from Nash's book (pg 34) to
+          understand this p, q, & r rotation business:
 
-		p = x"y; (" means transpose)
-		q = x"x - y"y;
-		v = sqrt(4*p^2+q^2);
-		if (q>=0) {
-		  cos(phi) = sqrt((v+q)/(2*v));
-		  sin(phi) = p/(v*cos(phi));
-		} else if (q<0) {,
-		  (sgn(p)=+1 for p>=0, -1 for p<0);
-		  sin(phi) = sgn(p)*sqrt((v-q)/(2*v));
-		  cos(phi) = p/(v*sin(phi));
-		}
-		This formulation avoids the subtraction of two nearly
-		equal numbers, which is bound to happen to q and v as
-		the matrix approaches orthogonality.
-	      */
-              for (i=0; i<nRow; i++)
-                {
-                  x0 = W[nCol*i+j]; y0 = W[nCol*i+k];
-                  p += x0*y0; q += x0*x0; r += y0*y0;
-                }
-              Z[j] = q; Z[k] = r;
-	      /* Convergence test: will rotation exchange order of columns?*/
-	      if (q >= r) /* check if columns are ordered */
-                { /* columns are ordered, so try convergence test */
-                  if (q<=e2*Z[0] || fabs(p)<=tol*q) RotCount--;
-		  /* There is no more work on this particular pair of
-		     columns in the current sweep. The first condition
-		     checks for very small column norms in BOTH
-		     columns, for which no rotation makes sense. The
-		     second condition determines if the inner product
-		     is small with respect to the larger of the
-		     columns, which implies a very small rotation
-		     angle. */
-                  else
-                    {/* columns are in order, but their inner product is not small */
-                      p /= q; r = 1 - r/q; vt = sqrt(4*p*p+r*r);
-		      /* DAL thinks the fabs is unnecessary in the
-			 next line: vt is non-negative; q>=r, r/q <=1
-			 so after the assignment 0<= r <=1, so r/vt is
-			 positive, so everything inside the sqrt
-			 should be positive even without the fabs. abs
-			 isn't in Nash's book. c0 and s0 are cos(phi) and sin(phi) as above for q>=0*/
-                      c0 = sqrt(fabs(.5*(1+r/vt))); s0 = p/(vt*c0);
-		      /* this little for loop (and the one below) is just rotation, inlined here for efficiency */
-                      for (i=0; i<nRow+nCol; i++)
-                        {
-                          d1 = W[nCol*i+j]; d2 = W[nCol*i+k];
-                          W[nCol*i+j] = d1*c0+d2*s0; W[nCol*i+k] = -d1*s0+d2*c0;
-                        }
-                    }
-                }
-              else /* columns out of order -- must rotate */
-                { /* note: r>q, and cannot be zero since both are sums
-		     of squares for the svd. In the case of a real
-		     symmetric matrix, this assumption must be
-		     questioned. */
-                  p /= r; q = q/r-1; vt = sqrt(4*p*p+q*q);
-                  s0 = sqrt(fabs(.5*(1-q/vt)));
-		  /* DAL wondering about the fabs again, since after
-		     assignment q is <0 and vt is again positive, so
-		     everything inside the fabs should be positive
-		     already */
-                  if (p<0) s0 = -s0; /*s0 and c0 are sin(phi) and cos(phi) as above for q<0 */
-                  c0 = p/(vt*s0);
-                  for (i=0; i<nRow+nCol; i++) /* rotation again */
-                    {
-                      d1 = W[nCol*i+j]; d2 = W[nCol*i+k];
-                      W[nCol*i+j] = d1*c0+d2*s0; W[nCol*i+k] = -d1*s0+d2*c0;
-                    }
-                }
-		  /* Both angle calculations have been set up so that
-		     large numbers do not occur in intermediate
-		     quantities. This is easy in the svd case, since
-		     quantities x2,y2 cannot be negative.*/
-            } /* loop on k */
-        } /* loop on j */
-      while (EstColRank>=3 && Z[(EstColRank-1)]<=Z[0]*tol+tol*tol)
-        EstColRank--;
-    }
+          p = x"y; (" means transpose)
+          q = x"x - y"y;
+          v = sqrt(4*p^2+q^2);
+          if (q>=0) {
+            cos(phi) = sqrt((v+q)/(2*v));
+            sin(phi) = p/(v*cos(phi));
+          } else if (q<0) {,
+            (sgn(p)=+1 for p>=0, -1 for p<0);
+            sin(phi) = sgn(p)*sqrt((v-q)/(2*v));
+            cos(phi) = p/(v*sin(phi));
+          }
+          This formulation avoids the subtraction of two nearly
+          equal numbers, which is bound to happen to q and v as
+          the matrix approaches orthogonality.
+        */
+        for (i=0; i<nRow; i++) {
+          x0 = W[nCol*i+j]; y0 = W[nCol*i+k];
+          p += x0*y0; q += x0*x0; r += y0*y0;
+        }
+        Z[j] = q; Z[k] = r;
+        /* Convergence test: will rotation exchange order of columns?*/
+        if (q >= r) { /* check if columns are ordered */
+          /* columns are ordered, so try convergence test */
+          if (q<=e2*Z[0] || fabs(p)<=tol*q) {
+            /* There is no more work on this particular pair of
+               columns in the current sweep. The first condition
+               checks for very small column norms in BOTH
+               columns, for which no rotation makes sense. The
+               second condition determines if the inner product
+               is small with respect to the larger of the
+               columns, which implies a very small rotation
+               angle. */
+            RotCount--;
+          } else {
+            /* columns are in order, but their inner product is not small */
+            p /= q; r = 1 - r/q; vt = sqrt(4*p*p+r*r);
+            /* DAL thinks the fabs is unnecessary in the
+               next line: vt is non-negative; q>=r, r/q <=1
+               so after the assignment 0<= r <=1, so r/vt is
+               positive, so everything inside the sqrt
+               should be positive even without the fabs. abs
+               isn't in Nash's book. c0 and s0 are cos(phi) and sin(phi) as above for q>=0*/
+            c0 = sqrt(fabs(.5*(1+r/vt))); s0 = p/(vt*c0);
+            /* this little for loop (and the one below) is just rotation, inlined here for efficiency */
+            for (i=0; i<nRow+nCol; i++) {
+              d1 = W[nCol*i+j]; d2 = W[nCol*i+k];
+              W[nCol*i+j] = d1*c0+d2*s0; W[nCol*i+k] = -d1*s0+d2*c0;
+            }
+          }
+        } else {
+        /* columns out of order -- must rotate */
+        /* note: r>q, and cannot be zero since both are sums
+             of squares for the svd. In the case of a real
+             symmetric matrix, this assumption must be
+             questioned. */
+          p /= r; q = q/r-1; vt = sqrt(4*p*p+q*q);
+          s0 = sqrt(fabs(.5*(1-q/vt)));
+          /* DAL wondering about the fabs again, since after
+             assignment q is <0 and vt is again positive, so
+             everything inside the fabs should be positive
+             already */
+          if (p<0) s0 = -s0; /*s0 and c0 are sin(phi) and cos(phi) as above for q<0 */
+          c0 = p/(vt*s0);
+          for (i=0; i<nRow+nCol; i++) { /* rotation again */
+            d1 = W[nCol*i+j]; d2 = W[nCol*i+k];
+            W[nCol*i+j] = d1*c0+d2*s0; W[nCol*i+k] = -d1*s0+d2*c0;
+          }
+        }
+        /* Both angle calculations have been set up so that
+           large numbers do not occur in intermediate
+           quantities. This is easy in the svd case, since
+           quantities x2,y2 cannot be negative.*/
+      } /* loop on k */
+    } /* loop on j */
+    while (EstColRank>=3 && Z[(EstColRank-1)]<=Z[0]*tol+tol*tol)
+      EstColRank--;
+  }
 #if DEBUG
   if (SweepCount > slimit)
     fprintf(stderr, "Sweeps = %d\n", SweepCount);
