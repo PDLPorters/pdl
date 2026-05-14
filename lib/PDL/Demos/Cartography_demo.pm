3c1741adb32eecb5df14d9832fadb239c3fe22db
Ed J <mohawk2@users.noreply.github.com>
2026-05-14 17:44:21 +0100

    add t_stereographic and t_sinusoidal to demo

--- lib/PDL/Demos/Cartography_demo.pm
+++ lib/PDL/Demos/Cartography_demo.pm
@@ -12,7 +12,7 @@ sub info {('cartography','Cartographic projections (Req.: PDL::Graphics::Simple)
 my @demos = (
 PDL->rpiccan('JPEG') ? () :
     [comment => q|
-This demo illustrates the PDL::Transform::Cartography module.  
+This demo illustrates the PDL::Transform::Cartography module.
 
 It requires PDL::Graphics::Simple and also the ability to read/write
 JPEG images.
@@ -20,7 +20,7 @@ JPEG images.
 You don't seem to have that ability at the moment -- this is likely
 because you do not have NetPBM installed.  See the man page for PDL::IO::Pic.
 
-I'll continue with the demo anyway, but it will likely crash on the 
+I'll continue with the demo anyway, but it will likely crash on the
 earth_image('day') call on the next screen.
 
 |],
@@ -31,15 +31,15 @@ earth_image('day') call on the next screen.
  Also you must have PDL::Graphics::Simple installed to run it.
 
  PDL::Transform::Cartography includes a global earth vector coastline map
- and night and day world image maps, as well as the infrastructure for 
+ and night and day world image maps, as well as the infrastructure for
  transforming them to different coordinate systems.
 |],
 
 [act => q|
-  ### Load the necessary modules 
+  ### Load the necessary modules
     use PDL::Graphics::Simple;
     use PDL::Transform::Cartography;
-    
+
   ### Get the vector coastline map (and a lon/lat grid), and load the Earth
   ### RGB daytime image -- both of these are built-in to the module. The
   ### coastline map is a set of (X,Y,Pen) vectors.
@@ -57,7 +57,7 @@ earth_image('day') call on the next screen.
   print "FITS HEADER INFORMATION:\n";
   for $_(sort keys %{$map->hdr}){
     next if(m/SIMPLE/ || m/HISTORY/ || m/COMMENT/);
-    printf ("  %8s: %10s%s", $_, $map->hdr->{$_}, (++$i%3) ? "  " : "\n"); 
+    printf ("  %8s: %10s%s", $_, $map->hdr->{$_}, (++$i%3) ? "  " : "\n");
   }
   print "\n";
 
@@ -70,7 +70,7 @@ earth_image('day') call on the next screen.
   ### on top of the window with the "with polylines" PDL::Graphics::Simple
   ### plot type.  The clean_lines method breaks lines that pass over
   ### the map's singularity at the 180th parallel.
-  
+
   $w->hold;
   $w->plot(with=>'polylines', $coast->clean_lines);
   $w->release;
@@ -78,7 +78,7 @@ earth_image('day') call on the next screen.
 &],
 
 [act => q&
-### There are a large number of map projections -- to list them all, 
+### There are a large number of map projections -- to list them all,
 ### say "??cartography" in the perldl shell.  Here are four
 ### of them:
 
@@ -116,10 +116,10 @@ draw( t_lambert(   o=>[-90,40] ), "Lambert ",[400,300],{or=>[[-3,3],[-2,2]]} );
 ### vertical, and true perspective.  The true perspective has options for
 ### both downward-looking and aerial-view coordinate systems.
 
-draw( t_orthographic( o=>[-90,40] ), 
+draw( t_orthographic( o=>[-90,40] ),
       "Orthographic",  [400,300]);
 
-draw( t_vertical( r0=> (2 + 1), o=>[-90,40] ), 
+draw( t_vertical( r0=> (2 + 1), o=>[-90,40] ),
       "Vertical (Altitude = 2 r_e)", [400,300]);
 
 draw( t_perspective( r0=> (2 + 1), o=>[-90,40] ),
@@ -133,19 +133,36 @@ draw( t_perspective( r0=> 1.1, o=>[-117,31], cam=>[-22,-45,0] ),
 
 |],
 
+[act => q|
+### Speaking of stereographic and gnomonic projections, here are others.
+
+draw( t_sinusoidal(),
+      "Sinusoidal",  [400,300]);
+
+draw( t_stereographic(),
+      "Stereographic",  [400,300]);
+
+draw( t_stereographic(o=>[0,90]),
+      "Stereographic about North Pole",  [400,300]);
+
+draw( t_az_eqa(),
+      "Azimuthal equal-area",  [400,300]);
+
+|],
+
 [comment => q|
 
 That concludes the basic cartography demo.  Numerous other transforms
-are available.  
+are available.
 
 Because PDL's cartographic transforms work within the Transform module
 and are invertible, it's easy to use them both forwards and backwards.
-In particular, the perspective transformation is useful for ingesting 
+In particular, the perspective transformation is useful for ingesting
 scientific image data of the Earth or other planets, and converting to
 a map of the imaged body.
 
-Similarly, scanned images of map data can easily be converted into 
-lat/lon coordinates or reprojected to make other projections. 
+Similarly, scanned images of map data can easily be converted into
+lat/lon coordinates or reprojected to make other projections.
 
 |],
 );
